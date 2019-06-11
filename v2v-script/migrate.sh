240d9a187fd6f034272e852a2aac59ffc659fa84
jyamamoto <javier.yamamoto@gmail.com>
2019-06-11 13:49:17 -0300

    pp

--- v2v-script/migrate.sh
+++ v2v-script/migrate.sh
@@ -7,25 +7,31 @@ if [ $# -eq 2 ] && [ -e $1 ] && [ -e $2 ] && [ -e $3 ]; then
 	customize_file=$3
 
 	#adaptacion de vm
+	echo "PASO 1 - Adantando VM para OSP"
 	virt-customize --run $customize_file --root-password password:redhat01 -a $image_name
 
 	#obteniendo el tamaño de la imagen
+	echo "PASO 2 - Obteniendo el tamaño de la imagen"
 	image_size=$(qemu-img info $image_name | awk ‘{if (NR==4) print $3+0}’)
 
 	#generando el volumen y almacenando el id
-	volume_id=$(cinder create --display-name $name $image_size --poll | awk ‘{if (NR==11) print $4}’)
+	echo "PASO 3 - Generando volumen y almacenando el id"
+	volume_id=$(cinder create --display-name $vm_name $image_size --poll | awk ‘{if (NR==11) print $4}’)
 
 	#se borra el volumen desde ceph dejando intactos los registros de openstack
+	echo "PASO 4 - Se borra el volumen desde ceph dejando intactos los registros de openstack"
 	rbd --user=openstack -p osp-volumes rm volume-$volume_id
 
 	#convirtiendo la imagen a ceph e inyectando el nuevo volumen
+	echo "PASO 5 - Convirtiendo la imagen a ceph e inyectando el nuevo volumen"
 	qemu-img convert -p $image_name -O rbd rbd:osp-volumes/volume-$volume_id:id=openstack
 
 	#declarando el volumen como booteable
+	echo "PASO 6 - Declarando el volumen como booteable"
 	cinder set-bootable $volume_id
 
 	#exponiendo el id de volumen por pantalla
-	echo $volume_id
+	echo "El siguiente VOLUMEN_ID $volume_id fue generado para la VM $vm_name" 
 else
 	echo “usage: migrate <vm_name> <image_file> <customize_file>”
 fi
