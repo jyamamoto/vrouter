ce1fa16addb843c159a4c88020f85a2f8d3021af
jyamamoto <javier.yamamoto@gmail.com>
2019-06-11 14:46:28 -0300

    uiui

---
+++ v2v-script/port_create.sh
@@ -0,0 +1,14 @@
+
+net=muni-net-priv
+subnet="192.168.150.0/24"
+ip_address=$2
+vm_name=$1
+
+if [ $# -eq 2 ]; then
+	openstack port create --network $net --fixed-ip subnet=$subnet,ip-address=$ip_address --no-security-group port-$vm_name
+else
+	echo "uso: $0 <vm_name> <ip_address>"
+fi
+
+# Por si se necesita el security group
+#   --security-group <security-group> | --no-security-group]
